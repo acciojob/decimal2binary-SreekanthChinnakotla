9e1ce9aa43d4be4e4b2e746cf4b5eec9c69d94c5
SreekanthChinnakotla <134213780+SreekanthChinnakotla@users.noreply.github.com>
2023-09-27 13:22:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,24 @@
 function decimalToBinary(num) {
-  //Write you code here
-	let binary=parseInt(num,2);
-	return binary;
+  // Check if the input number is 0
+  if (num === 0) {
+    return '0';
+  }
+
+  let binary = '';
   
+  while (num > 0) {
+    // Get the remainder when dividing by 2 (0 or 1)
+    const remainder = num % 2;
+    
+    // Prepend the remainder to the binary string
+    binary = remainder + binary;
+    
+    // Integer division by 2
+    num = Math.floor(num / 2);
+  }
+
+  return binary;
 }
 
+// Export the function
 window.decimalToBinary = decimalToBinary;
